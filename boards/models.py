0a72a9e7439a3629cf207c20b77b0c05aea60cac
yoonwoo <lkkjasd@naver.com>
2019-03-12 05:05:29 +0000

    01_Comment 모델 생성

--- boards/models.py
+++ boards/models.py
@@ -9,3 +9,16 @@ class Board(models.Model):
     
     def __str__(self):
         return f'{self.id}: {self.title}'
+
+class Comment(models.Model):
+    content = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True) # 만들어질때 1번
+    updated_at = models.DateTimeField(auto_now=True) # 저장될때만 1번
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    # on_delete는 참조하는 객체가 사라질 경우 설정
+    # CASCADE : 같이 삭제
+    # SET_NULL : NULL 값으로 변경(NOT NULL인 경우 불가능)
+    # SET_DEFAULT : DEFAULT으로 변경(DEFAULT값 없으면 불가능)
+    # PROTECT : 삭제 불가
+    def __str__(self):
+        return self.content
